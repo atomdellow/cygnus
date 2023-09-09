83947d31d38f150de8d53f30b7b88289d927e8b6
atomdellow <atomdellow@gmail.com>
2023-09-09 02:05:55 -0400

    Adjusted content so that it appears in the Git Repo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cygnus
-Studio site for Cygnus XR, supporting the development and marketing of the VR and XR projects they created 
+Studio site for Cygnus XR, supporting the development and marketing of the VR and XR projects they created.
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/gameStudio', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(express.json());
+
+// Routes would be added here
+//
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
