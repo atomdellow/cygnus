e34b2592a433ef30374e36fcdc2a20f19f0582f9
Adam Dellow <atomdellow@gmail.com>
2023-09-12 01:00:52 -0400

    added and completed navbar and styling

--- Cygnus/src/components/Header.vue
+++ Cygnus/src/components/Header.vue
@@ -1,9 +1,36 @@
 <template>
-  <nav>
-    <!-- Header menu with links to various pages -->
+  <nav class="navbar">
+    <a href="#">Cygnus</a>
+    <a href="#">Worlds</a>
+    <a href="#">Projects</a>
+    <a href="#">Patreon</a>
+    <a href="#">Blog</a>
+    <a href="#">Contact</a>
   </nav>
 </template>
+<style>
+/* Style for the header */
+.navbar {
+  width: 100%; /* Take up the full width of its container */
+  display: flex;
+  justify-content: center; /* Horizontally center navbar content */
+  align-items: center; /* Vertically center navbar content */
+  height: 60px; /* or whatever height you prefer */
+  /* other navbar styles */
+}
 
+/* Style for the links */
+.navbar a {
+  text-decoration: none;
+  color: black; /* or any color you prefer */
+  padding: 10px 15px; /* Adjust as needed */
+}
+
+.navbar a:hover {
+  background-color: #555; /* or any hover color you prefer */
+}
+
+</style>
 <script>
 export default {
   name: 'Header'
--- Cygnus/src/views/HomePage.vue
+++ Cygnus/src/views/HomePage.vue
@@ -1,10 +1,17 @@
 <template>
   <section>
-    <h1>Welcome to Game Studio</h1>
+    <h1>Welcome to Cygnus Studio</h1>
     <!-- More content -->
   </section>
 </template>
-
+<style>
+.homepage {
+  flex-grow: 1; /* Allow it to take up all available space */
+  display: flex;
+  justify-content: center; /* Horizontally center */
+  align-items: center; /* Vertically center */
+}
+</style>
 <script>
 export default {
   name: 'HomePage'
